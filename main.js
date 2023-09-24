2725d2d1e2ca51a99965a2f72bc7164c30f03d7c
Narges.Abolhasan <narges.abolhasan@gmail.com>
2023-09-24 15:29:18 +0330

    add redirect to iranSib

---
+++ main.js
@@ -0,0 +1,11 @@
+function succsessHandler(){
+
+    //window.open("http://localhost:3000/ResultPayment?success");
+    window.location.href = "http://react-iransib.onrender.com/ResultPayment?success";
+}
+
+function faileHandler(){
+   // window.open('http://localhost:3000/ResultPayment?failer');
+    window.location.href = 'http://react-iransib.onrender.com/ResultPayment?failer';
+ 
+}
